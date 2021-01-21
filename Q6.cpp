c9763619beea9fbc1eae48f48f63ea410ed1e672
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-01-22 02:14:55 +0530

    Create Q6.cpp

---
+++ Q6.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+#include<conio.h>
+main()
+{	int n,i,j,p;
+	clrscr();
+	cout<<"\nEnter no of N ";
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{	p=0;
+		for(j=2;j<=i/2;j++)
+		if(i%j==0)
+		 p=1;
+	if(p==0)
+	cout<<"  "<<i;
+	}
+getch();
+}
