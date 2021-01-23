2053a6a38d7dcd83689ef8873dc75dee6d01f1e3
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-01-23 22:58:41 +0530

    Create Q14.cpp

---
+++ Q14.cpp
@@ -0,0 +1,28 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{	int a[20],n,i,j,t;
+	clrscr();
+	cout<<"\nEnter no of elements in A ";
+	cin>>n;
+	for(i=0;i<n;i++)
+	{	cout<<"\nEnter no ";
+		cin>>a[i];
+	}
+	cout<<"\n\n";
+	for(i=0;i<n;i++)
+		cout<<" "<<a[i];
+	for(i=0;i<n;i++)
+	{	for(j=i+1;j<n;j++)
+			if(a[i]>a[j])
+			 {	t=a[i];
+				a[i]=a[j];
+				a[j]=t;
+			 }
+	}
+              
+
+	  cout<<"\n\n";
+	for(i=0;i<n;i++)
+		cout<<" "<<a[i];
+getch();}
