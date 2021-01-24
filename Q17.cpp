a5999edafdcad8fdd5600b52adcb236d79055a0d
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-01-24 23:19:32 +0530

    Create Q17.cpp

---
+++ Q17.cpp
@@ -0,0 +1,22 @@
+#include<iostream.h>
+#include<fstream.h>
+#include<conio.h>
+main()
+{	char ch;
+	clrscr();
+	ofstream f1("emp");    		//implicit
+	f1.open("emp",ios::out);		//explicit
+	cout<<"\nEnter char ";
+	while(1)		//infinity
+	{	ch=getche();	// to input a single charactor by keybord.
+		if(ch=='*')
+		break;
+		if(ch==13)
+		{	cout<<"\n";
+		 	f1<<'\n';
+		 }
+		f1<<ch;
+	}
+f1.close();
+getch();
+}
