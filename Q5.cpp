bca2e861dffacdf533fb69cad7a81a0bb5f205a1
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-02-06 18:38:38 +0530

    Update Q5.cpp

--- Q5.cpp
+++ Q5.cpp
@@ -5,14 +5,16 @@
 #include<fstream.h>
 #include<stdio.h>
 class abc
-{	private:
+{	
+	private:
 	int empno;
 	char n[20];
 	int age;
 	float sal;
-public:
+	public:
 	void getdata()
-	{	cout<<"\nEnter employee no ";
+	{	
+		cout<<"\nEnter employee no ";
 		cin>>empno;
 		cout<<"\nEnter name ";
 		cin>>n;
@@ -21,17 +23,19 @@ public:
 		cout<<"\nEnter salary ";
 		cin>>sal;
 	}
-	void putdata()
 	
-{
+	void putdata()
+	{
 		cout<<"\n\n"<<empno<<"\t"<<n<<"\t"<<age<<"\t"<<sal;
 	}
 	int get_empno()
-	{		return empno;
+	{	
+		return empno;
 	}
 };
 main()
-{	abc p,p1;
+{	
+	abc p,p1;
 	clrscr();
 	ifstream f1("emp5.txt",ios::in|ios::binary);
 	ofstream f2("temp",ios::out|ios::binary);
@@ -41,35 +45,42 @@ main()
 	f1.seekg(0);
 	for(i=0;i<4;i++)
 	while(!f1.eof())
-	{	f1.read((char*)&p1,sizeof(p1));
+	{	
+		f1.read((char*)&p1,sizeof(p1));
 		if(f1.eof())
 		break; 
 		p1.putdata();
-}
-cout<<"\nEnter employee no ";
-cin>>emp;
-f1.clear();
-f1.seekg(0);
-while(!f1.eof())
-{          f1.read((char*)&p1,sizeof(p1));
-	if(f1.eof())
-	break;
-	if(emp!=p1.get_empno())
-	f2.write((char*)&p1,sizeof(p1));
-}
-f1.close();
-f2.close();
-remove("emp5.txt");
-rename("temp","emp5.txt");
-f1.open("emp5.txt",ios::in|ios::binary);
-cout<<"\n\nNAME\tAGE\tSALARY";
-f1.seekg(0);
-while(!f1.eof())
-{	f1.read((char*)&p1,sizeof(p1));
-	if(f1.eof())
-	break;
-	p1.putdata();
-}
-f1.close();
-getch();
+	}
+
+	cout<<"\nEnter employee no ";
+	cin>>emp;
+	f1.clear();
+	f1.seekg(0);
+
+	while(!f1.eof())
+	{       
+		f1.read((char*)&p1,sizeof(p1));
+		if(f1.eof())
+		break;
+		if(emp!=p1.get_empno())
+		f2.write((char*)&p1,sizeof(p1));
+	}
+
+	f1.close();
+	f2.close();
+	remove("emp5.txt");
+	rename("temp","emp5.txt");
+	f1.open("emp5.txt",ios::in|ios::binary);
+	cout<<"\n\nNAME\tAGE\tSALARY";
+	f1.seekg(0);
+
+	while(!f1.eof())
+	{	
+		f1.read((char*)&p1,sizeof(p1));
+		if(f1.eof())
+		break;
+		p1.putdata();
+	}
+	f1.close();
+	getch();
 }
