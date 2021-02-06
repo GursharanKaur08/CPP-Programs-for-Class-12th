a4c82e4109688ea613b0bf0570ddb0b3f316dae5
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-02-06 18:44:54 +0530

    Update Q3.cpp

--- Q3.cpp
+++ Q3.cpp
@@ -3,10 +3,12 @@
 #include<iostream.h>
 #include<conio.h>
 #include<string.h>
+
 main()
 {	
             char n[20],n1[20],n2[20];
             int  l1,l2,l3;	
+           
             clrscr();
 
             cout<<"\nEnter String1 ";
@@ -23,5 +25,6 @@ main()
             (l1>l2 && l1>l3) ?cout<<"\nLong: "<<n:(l2>l1  && l2>l3) ? cout<<"\nLong: "<<n1 :cout<<"\nLong: "<<n2;
 
             (l1<l2 && l1<l3)?cout<<"\nshort:"<<n:(l2<l1  && l2<l3)?cout<<"\nshort:"<<n1: cout<<"\nshort:"<<n2;
-getch();
+
+            getch();
 }
