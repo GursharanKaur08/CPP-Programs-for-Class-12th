6c8964ab582950d8ec604d1747025fd27dcbcd2f
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-02-06 18:33:07 +0530

    Update Q9.cpp

--- Q9.cpp
+++ Q9.cpp
@@ -3,30 +3,38 @@
 #include<iostream.h>
 #include<conio.h>
 void twodigit(int a[10][10],int m,int n)
-{	int i,j,flag=0;
+{	
+	int i,j,flag=0;
 	cout<<"The Two digit Numbers are: ";
 	for(i=0;i<m;i++)
+	{
 		for(j=0;j<n;j++)
-		{	if(a[i][j]>=10&&a[i][j]<=99)
-			{	cout<<a[i][j]<<ends;
+		{	
+			if(a[i][j]>=10&&a[i][j]<=99)
+			{	
+				cout<<a[i][j]<<ends;
 				flag=1;
 			}
 		}
 		if(flag==0)
 		cout<<"None";
+	}
 }
 void main()
-{	clrscr();
+{	
+	clrscr();
 	int a[10][10],i,j,m,n;
 	cout<<"\n EnterNo. of rows: ";
 	cin>>m;
-	 cout<<"\nEnter the no. of columns: ";
-           	cin>>n;
+	cout<<"\nEnter the no. of columns: ";
+        cin>>n;
 	
-cout<<"\nEnter the Elements of the array: ";
+        cout<<"\nEnter the Elements of the array: ";
 	for(i=0;i<m;i++)
+	{
 		for(j=0;j<n;j++)
 		cin>>a[i][j];
+	}
 	twodigit(a,m,n);
 	getch();
 }
