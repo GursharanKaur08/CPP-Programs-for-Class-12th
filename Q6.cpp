50d164bb6e6da6851e85432515a222df9732d48b
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-02-06 18:35:52 +0530

    Update Q6.cpp

--- Q6.cpp
+++ Q6.cpp
@@ -3,17 +3,21 @@
 #include<iostream.h>
 #include<conio.h>
 main()
-{	int n,i,j,p;
+{	
+	int n,i,j,p;
 	clrscr();
 	cout<<"\nEnter no of N ";
 	cin>>n;
 	for(i=1;i<=n;i++)
-	{	p=0;
+	{	
+		p=0;
 		for(j=2;j<=i/2;j++)
-		if(i%j==0)
-		 p=1;
-	if(p==0)
-	cout<<"  "<<i;
+		{
+			if(i%j==0)
+		 	p=1;
+		}
+		if(p==0)
+		cout<<"  "<<i;
 	}
-getch();
+	getch();
 }
