e5af760034e58f584a983f52aaa0c7ae538e3916
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-02-16 19:05:42 +0530

    Create Q6.cpp

---
+++ Program Code(1-10)/Q6.cpp
@@ -0,0 +1,19 @@
+//Enter any number and the output will be all the prime numbers up to that number.
+
+#include<iostream.h>
+#include<conio.h>
+main()
+{	int n,i,j,p;
+	clrscr();
+	cout<<"\nEnter no of N ";
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{	p=0;
+		for(j=2;j<=i/2;j++)
+		if(i%j==0)
+		 p=1;
+	if(p==0)
+	cout<<"  "<<i;
+	}
+getch();
+}
