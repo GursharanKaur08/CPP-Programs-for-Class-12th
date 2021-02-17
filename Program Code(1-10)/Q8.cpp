54b0ca43a583ff5ffd429525952e445c7ec40213
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-02-17 13:54:20 +0530

    Update Q8.cpp

--- Program Code(1-10)/Q8.cpp
+++ Program Code(1-10)/Q8.cpp
@@ -3,15 +3,17 @@
 #include<iostream.h>
 #include<conio.h>
 main()
-{	int a=0,b=0,c;
+{	
+	int a=0,b=0,c;
 	clrscr();
 	cout<<"\nEnter value for A ";
 	cin>>a;
 	while(a>0)	//	for(;a>0;)  or for(i=a;i>0;i=i/10)
-	{	c=a%10;
+	{	
+		c=a%10;
 		b=b+c;
 		a=a/10;
-             }	
+    }	
 	cout<<"\nSum of digits "<<b;
 	getch();
 }
