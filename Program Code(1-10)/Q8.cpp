b00c82aecbfdc13324f4ce775dd62bd05990955a
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-02-17 13:10:49 +0530

    Create Q8.cpp

---
+++ Program Code(1-10)/Q8.cpp
@@ -0,0 +1,17 @@
+//Enter any number from 2 digit to 5 digit and the output will be the sum of all the distinguish digits of the numbers.
+
+#include<iostream.h>
+#include<conio.h>
+main()
+{	int a=0,b=0,c;
+	clrscr();
+	cout<<"\nEnter value for A ";
+	cin>>a;
+	while(a>0)	//	for(;a>0;)  or for(i=a;i>0;i=i/10)
+	{	c=a%10;
+		b=b+c;
+		a=a/10;
+             }	
+	cout<<"\nSum of digits "<<b;
+	getch();
+}
