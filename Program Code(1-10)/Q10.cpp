61d0c9ddd628a585d76f3a4202456c2f870b8aaa
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-02-17 13:57:24 +0530

    Update Q10.cpp

--- Program Code(1-10)/Q10.cpp
+++ Program Code(1-10)/Q10.cpp
@@ -3,27 +3,32 @@
 #include<iostream.h>
 #include<conio.h>
 main()
-{	int a[20],n,i,no,low=0,high,mid,f=0;
+{	
+	int a[20],n,i,no,low=0,high,mid,f=0;
 	clrscr();
 	cout<<"\nEnter no of elements ";
 	cin>>n;
+	
 	for(i=0;i<n;i++)
-	{	cout<<"\nEnter no ";
+	{	
+		cout<<"\nEnter no ";
 		cin>>a[i];
 	}
 	cout<<"\n\n";
 	for(i=0;i<n;i++)
 		cout<<"  "<<a[i];
+
 cout<<"\nEnter no to be search ";
 cin>>no;
 high=n-1;
-while(low<=high && f==0)
-	{	mid=(low+high)/2;
+	while(low<=high && f==0)
+	{	
+		mid=(low+high)/2;
 		if (a[mid]==no)
-		{	cout<<"\n\n"<<no<<" store at "<<mid+1;
-		
-		f=1;
-		break;
+		{	
+			cout<<"\n\n"<<no<<" store at "<<mid+1;
+			f=1;
+			break;
 		}
 		else  if(no>a[mid])
 			low=mid+1;
