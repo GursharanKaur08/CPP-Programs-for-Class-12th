472744571399d930f6fe1c96dba1262d426da456
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-02-20 23:04:05 +0530

    Update Q15.cpp

--- Program Code(11-20)/Q15.cpp
+++ Program Code(11-20)/Q15.cpp
@@ -3,18 +3,22 @@
 #include<iostream.h>
 #include<conio.h>
 void main()
-{	int a[20],n,i,j,t,min,p;
+{	
+    int a[20],n,i,j,t,min,p;
 	clrscr();
 	cout<<"\nEnter no of elements in A ";
 	cin>>n;
-	for(i=0;i<n;i++)
+	
+    for(i=0;i<n;i++)
 	{	cout<<"\nEnter no ";
 		cin>>a[i];
 	}
 	cout<<"\n\n";
-	for(i=0;i<n;i++)
+	
+    for(i=0;i<n;i++)
 		cout<<" "<<a[i];
-	for(i=0;i<n;i++)
+	
+    for(i=0;i<n;i++)
 	{	min=a[i];
 		p=i;
 		for(j=i;j<n;j++)
@@ -29,7 +33,9 @@ void main()
 			a[p]=t;
 		}
 	cout<<"\n\n";
-	for(i=0;i<n;i++)
+	
+    for(i=0;i<n;i++)
 		cout<<" "<<a[i];
-getch();
+
+    getch();
 }
