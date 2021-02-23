4cea45a0b4821ae8a824b6efb25a4580f1220de5
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-02-23 23:32:47 +0530

    Create Q17.cpp

---
+++ Program Code(11-20)/Q17.cpp
@@ -0,0 +1,24 @@
+//Enter any line or character in a file and press * to exit the program.
+
+#include<iostream.h>
+#include<fstream.h>
+#include<conio.h>
+main()
+{	char ch;
+	clrscr();
+	ofstream f1("emp");    		//implicit
+	f1.open("emp",ios::out);		//explicit
+	cout<<"\nEnter char ";
+	while(1)		//infinity
+	{	ch=getche();	// to input a single charactor by keybord.
+		if(ch=='*')
+		break;
+		if(ch==13)
+		{	cout<<"\n";
+		 	f1<<'\n';
+		 }
+		f1<<ch;
+	}
+f1.close();
+getch();
+}
