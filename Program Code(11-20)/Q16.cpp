954fe270025b9ad952ec82c4e4965d456a4af9b5
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-02-24 23:59:41 +0530

    Update Q16.cpp

--- Program Code(11-20)/Q16.cpp
+++ Program Code(11-20)/Q16.cpp
@@ -2,23 +2,31 @@
 
 #include<iostream.h>
 #include<conio.h>
- void main()
-{ 	int a[10][10],i,j,r,c;
+
+void main()
+{ 	
+	int a[10][10],i,j,r,c;
 	clrscr();
 	cout<<"\nEnter no of rows ";
 	cin>>r;
 	cout<<"\nEnter no of Col ";
 	cin>>c;
 	for(i=0;i<r;i++)
+	{
 		for(j=0;j<c;j++)
-		{	cout<<"\nEnter no ";
+		{	
+			cout<<"\nEnter no ";
 			cin>>a[i][j];
 		}
+	}
+
 	int sum=0,sum1=0;
 	for(i=0;i<r;i++)
-	{	cout<<"\n";
+	{	
+		cout<<"\n";
 		for(j=0;j<c;j++)
-		{	cout<<" "<<a[i][j];
+		{	
+			cout<<" "<<a[i][j];
 		    	If((i==j) && a[i][j]>0)
 			cout<<a[i][j];
 		       	else
@@ -31,13 +39,16 @@
 				cout<<" ";
 			if(i==j)
 				sum1=sum1+a[i][j];
-		  }
+		}
 	}
+
 	for(i=0;i<r;i++)
-	{	cout<<"\n";
+	{
+		cout<<"\n";
 		for(j=0;j<c;j++)
-		{	cout<<" "<<a[i][j];
-		     	if(i==j) //&& a[i][j]>0)
+		{	
+			cout<<" "<<a[i][j];
+		    if(i==j) //&& a[i][j]>0)
 				cout<<a[i][j];
 			else
 				cout<<" ";
@@ -49,7 +60,7 @@
 				sum=sum+a[i][j];
 		}
 	}
- cout<<"\n\nFirst diagonal sum "<<sum1;
- cout<<"\n\nSecond diagonal sum "<<sum;
-getch();
+ 	cout<<"\n\nFirst diagonal sum "<<sum1;
+ 	cout<<"\n\nSecond diagonal sum "<<sum;
+	getch();
 }
